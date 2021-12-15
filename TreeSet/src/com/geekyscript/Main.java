fad55a43d48587c6f56285f98c6575b4b8a4c9e3
_isha_30 <choudhary.isha30@gmail.com>
2021-12-15 23:28:19 +0530

    Functions

--- TreeSet/src/com/geekyscript/Main.java
+++ TreeSet/src/com/geekyscript/Main.java
@@ -10,6 +10,7 @@ public class Main {
         s.add("mango");
         s.add("apple");
         s.add("orange");
+        System.out.println("--------Print, contains, iterator--------");
         System.out.println(s);
 
         System.out.println(s.contains("orange"));
@@ -17,5 +18,18 @@ public class Main {
         Iterator<String> i = s.iterator();
         while(i.hasNext())
             System.out.println(i.next());
+
+        System.out.println("--------remove, for loop--------");
+
+        TreeSet<Integer> s1 = new TreeSet<>();
+        s1.add(15);
+        s1.add(10);
+        s1.add(2);
+        s1.add(5);
+        s1.add(18);
+
+        s1.remove(10);
+        for(Integer x : s1)
+            System.out.print(x + " ");
     }
 }
